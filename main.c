fb8691694516be46eb7d3091545858a10a792848
kkeian <keian.kaserman.me@gmail.com>
2024-12-08 16:51:04 -0500

    Add memory init sections to copy .data and init .bss

--- main.c
+++ main.c
@@ -7,6 +7,11 @@ int main(void)
 
 __attribute__((naked, noreturn)) void _reset(void)
 {
+    // Initialize memory regions
+    extern uint32_t *_sbss, *_ebss, *_sdata, *_edata, *_sidata, *_estack;
+    while (_sbss < _ebss) *_sbss++ = 0; /* zero init .bss section */
+    while (_sdata < _edata) *_sidata++ = *_sdata++; /* copy data from flash to RAM before program init */
+
     // init MCU and board (setup runtime env + clock setup and system configuration)
     init();
 
